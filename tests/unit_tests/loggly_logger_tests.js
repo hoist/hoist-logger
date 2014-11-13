13a675a164353a11f19f954065405ed2fd4c1542
Owen Evans <owen@bgeek.net>
2014-11-13 14:11:54 +1300

    inital hoist logger

---
+++ tests/unit_tests/loggly_logger_tests.js
@@ -0,0 +1,47 @@
+'use strict';
+require('../bootstrap');
+var loggly = require('loggly');
+var LogglyLogger = require('../../lib/loggly_logger');
+var sinon = require('sinon');
+var expect = require('chai').expect;
+describe('LogglyLogger', function () {
+  var stubLoggly ={
+    log:sinon.stub(),
+    reset:function(){
+      this.log.reset();
+    }
+  };
+  var _logger;
+  var _clock;
+  before(function(){
+    _clock = sinon.useFakeTimers(Date.now());
+    sinon.stub(loggly,'createClient').returns(stubLoggly);
+    _logger = new LogglyLogger();
+  });
+  afterEach(function(){
+    stubLoggly.reset();
+  });
+  after(function(){
+    _clock.restore();
+    loggly.createClient.restore();
+  });
+  it('creates loggly client with correct params',function(){
+    return expect(loggly.createClient)
+    .to.have.been.calledWith({
+      token:'TEST_LOGGLY_TOKEN',
+      subdomain:'TEST_LOGGLY_DOMAIN',
+      json:true,
+      tags:['1','2']
+    });
+
+  });
+  describe('#write',function(){
+    before(function(){
+      return _logger.write({time:Date.now()});
+    });
+    it('writes log to loggly',function(){
+      return expect(stubLoggly.log)
+      .to.have.been.calledWith([{timestamp:Date.now()}]);
+    });
+  });
+});
